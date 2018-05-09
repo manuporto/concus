a72fcb1bfbfbb59253144a70a0f7570fd84a3cd0
Manuel Porto <manu.porto94@gmail.com>
2018-05-09 20:01:03 -0300

    Agregar locks

--- Locks/ej3.c
+++ Locks/ej3.c
@@ -10,7 +10,18 @@ static const char* ARCHIVO_OUTPUT = "salida.txt";
 
 void escribir_archivo(int numero) {
     pid_t pid = getpid();
+
+    int lock_fd = open(ARCHIVO_LOCK, O_CREAT | O_WRONLY, 0777);
+    struct flock fl;
+    fl.l_type = F_WRLCK;
+    fl.l_whence = SEEK_SET;
+    fl.l_start = 0;
+    fl.l_len = 0;
+
     for (size_t i = 0; i < ITERACIONES; i++) {
+        fl.l_type = F_WRLCK;
+        fcntl(lock_fd, F_SETLKW, &fl);
+
         int fd = open(ARCHIVO_OUTPUT, O_RDONLY, 0777);
         int nro_leido;
         read(fd, &nro_leido, sizeof(int));
@@ -21,6 +32,9 @@ void escribir_archivo(int numero) {
         int nro_a_escribir = nro_leido + numero;
         write(fd, &nro_a_escribir, sizeof(int));
         close(fd);
+
+        fl.l_type = F_UNLCK;
+        fcntl(lock_fd, F_SETLK, &fl);
         printf("[%d] Escribi un %d\n", pid, nro_a_escribir);
     }
 }
